af8f2257b75acde6aaf89aa2d4de6a2198fc1a07
hasan082 <dr.has82@gmail.com>
2024-01-17 03:24:04 +0600

    1-100 Happy number

--- HappyNumber.js
+++ HappyNumber.js
@@ -1,16 +1,27 @@
 // A happy number 
 var isHappy = function (n) {
-    if(n==1){
+    if (n == 1) {
         return true;
-    }else if(n===4){
+    } else if (n === 4) {
         return false
     } else {
         let s = 0;
-            let a = n.toString().split('');
-            for (let i = 0; i < a.length; i++) {
-                s += a[i] * a[i];
-            }
-            n = s;
+        let a = n.toString().split('');
+        for (let i = 0; i < a.length; i++) {
+            s += a[i] * a[i];
+        }
+        n = s;
     }
     return isHappy(n);
 };
+
+// Program to print all Happy numbers between 1 to 100
+
+
+for (let i = 1; i <= 100; i++) {
+    let res = isHappy(i);
+    if(res == true){
+        console.log(i);//all Happy number
+    }
+    
+}
