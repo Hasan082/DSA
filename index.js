d62a72d354108e34ef7f76c67cc0244b73bd4cc8
hasan082 <dr.has82@gmail.com>
2024-01-17 02:59:33 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var calculateSumOfPowerDigits = function(n) {
+    let dtos = n.toString();
+    let ln = dtos.length;
+    let sum = 0;
+
+    for (let i = 0; i < ln; i++) {
+        let digit = parseInt(dtos[i]);
+        let power = i + 1;
+        let poweredDigit = 1;
+
+        for (let j = 0; j < power; j++) {
+            poweredDigit *= digit;
+        }
+
+        sum += poweredDigit;
+    }
+
+    return sum;
+};
+
+let result = calculateSumOfPowerDigits(48);
+console.log(result); // Output: 175 (1^1 + 7^2 + 5^3)
+
