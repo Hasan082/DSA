269a9997299127a8cc2a67021347e3af438836ed
Md Hasanuzzaman <dr.has82@gmail.com>
2024-04-29 23:03:26 +0600

    Leetcode 434 done

---
+++ dsaJava/leetcdoe434.java
@@ -0,0 +1,19 @@
+package dsaJava;
+
+public class leetcdoe434 {
+    public static int countSegments(String s) {
+        if (s.length() < 1)
+            return 0;
+        int segment = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if ((i == 0 || s.charAt(i - 1) == ' ') && s.charAt(i) != ' ') {
+                segment++;
+            }
+        }
+        return segment;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countSegments("Hello, my name is John"));
+    }
+}
