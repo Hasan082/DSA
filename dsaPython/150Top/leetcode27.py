771950036927ed0da3732bb64d5a32b2da877e54
Md Hasanuzzaman <dr.has82@gmail.com>
2024-12-30 23:59:49 +0600

    Update 27 Problem

--- dsaPython/150Top/leetcode27.py
+++ dsaPython/150Top/leetcode27.py
@@ -1,11 +1,10 @@
 def removeElement(nums, val):
-    count = nums.count(val)
-    # count = 0
-    # for i in range(len(nums)):
-    #     if nums[i] != val:
-    #         nums[count] = nums[i]
-    #         count += 1
-    return nums.count(val)
+    count = 0
+    for num in nums:
+        if num != val:
+            nums[count] = num
+            count += 1
+    return count
 
 
 nums1 = [3, 2, 2, 3]
