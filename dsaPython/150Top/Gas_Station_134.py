1ef42b835de7c0beb7f95b6e678f97069c6f3e32
Md Hasanuzzaman <dr.has82@gmail.com>
2025-01-02 02:10:31 +0600

    Gast station done

--- dsaPython/150Top/Gas_Station_134.py
+++ dsaPython/150Top/Gas_Station_134.py
@@ -3,16 +3,18 @@ from typing import List
 
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        start, total, current = 0, 0, 0
+        start, total_gas, total_cost, current = 0, sum(gas), sum(cost), 0
+
+        if total_gas < total_cost:
+            return -1
+
         for i in range(len(gas)):
-            gas_gain = gas[i] - cost[i]
-            total += gas_gain
-            current += gas_gain
-            if current >= 0:
+            current += gas[i] - cost[i]
+            if current < 0:
                 current = 0
                 start = i + 1
 
-        return -1 if total < 0 else start
+        return start
 
 
 res = Solution().canCompleteCircuit(gas=[1, 2, 3, 4, 5], cost=[3, 4, 5, 1, 2])
