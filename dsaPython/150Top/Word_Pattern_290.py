dec40943df93d1d6b539d270853638581fd63d0f
Md Hasanuzzaman <dr.has82@gmail.com>
2025-01-20 23:43:39 +0600

    Modify the file name

--- dsaPython/150Top/Word_Pattern_290.py
+++ dsaPython/150Top/Word_Pattern_290.py
@@ -1,10 +1,10 @@
-def wordPattern( pattern: str, s: str) -> bool:
+def wordPattern(pattern: str, s: str) -> bool:
     t = s.split()
     if len(t) != len(pattern):
         return False
     return len(set(zip(t, pattern))) == len(set(t)) == len(set(pattern))
 
 
-pattern = "abba"
-s = "dog cat cat dog"
-print(wordPattern(pattern, s)) # True
+p1 = "abba"
+s1 = "dog cat cat dog"
+print(wordPattern(p1, s1)) # True
