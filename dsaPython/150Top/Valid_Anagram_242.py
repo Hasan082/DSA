98a89f81c589bcc8c9f94bf15eb61934a39301fe
Md Hasanuzzaman <dr.has82@gmail.com>
2025-01-21 00:06:32 +0600

    File reformate

--- dsaPython/150Top/Valid_Anagram_242.py
+++ dsaPython/150Top/Valid_Anagram_242.py
@@ -1,14 +1,16 @@
-def isAnagram(self, s: str, t: str) -> bool:
-    if len(s) != len(t): return False
+def isAnagram(s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
     for char in set(s):
-        if s.count(char) != t.count(char): return False
+        if s.count(char) != t.count(char):
+            return False
     return True
 
 
 s1 = "anagram"
 t1 = "nagaram"
-print(isAnagram(s1, t1)) # True
+print(isAnagram(s1, t1))  # True
 
 s2 = "rat"
 t2 = "car"
-print(isAnagram(s2, t2)) # False
+print(isAnagram(s2, t2))  # False
