f12046e2edc7b0b67089f2bb39964c101ac6461d
Md Hasanuzzaman <dr.has82@gmail.com>
2025-01-20 23:54:19 +0600

    Solution added

--- dsaPython/150Top/Valid_Anagram_242.py
+++ dsaPython/150Top/Valid_Anagram_242.py
@@ -5,3 +5,10 @@ def isAnagram(self, s: str, t: str) -> bool:
     return True
 
 
+s1 = "anagram"
+t1 = "nagaram"
+print(isAnagram(s1, t1)) # True
+
+s2 = "rat"
+t2 = "car"
+print(isAnagram(s2, t2)) # False
