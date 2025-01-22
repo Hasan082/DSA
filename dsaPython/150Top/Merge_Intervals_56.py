99cf8b43f7827a3a78688d3a66be40d30804192c
Md Hasanuzzaman <dr.has82@gmail.com>
2025-01-22 20:32:55 +0600

    Solution added

--- dsaPython/150Top/Merge_Intervals_56.py
+++ dsaPython/150Top/Merge_Intervals_56.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+def merge(intervals: List[List[int]]) -> List[List[int]]:
     if len(intervals) < 2:
         return intervals
     intervals.sort(key=lambda i: i[0])
@@ -14,3 +14,9 @@ def merge(self, intervals: List[List[int]]) -> List[List[int]]:
             if r[-1][1] < interval[1]:
                 r[-1][1] = interval[1]
     return r
+
+
+intervals1 = [[1, 3], [2, 6], [8, 10], [15, 18]]
+print(merge(intervals1))
+intervals2 = [[1, 4], [4, 5]]
+print(merge(intervals2))
