288a1c7271c55d0af71938ac8cfc60a3150e9af6
Md Hasanuzzaman <dr.has82@gmail.com>
2025-01-22 19:30:00 +0600

    Solution added

--- dsaPython/150Top/Contains_Duplicate_II_219.py
+++ dsaPython/150Top/Contains_Duplicate_II_219.py
@@ -11,3 +11,17 @@ def containsNearbyDuplicate(nums: List[int], k: int) -> bool:
         d[n] = i
 
     return False
+
+
+nums1 = [1, 2, 3, 1]
+k1 = 3
+
+nums2 = [1, 0, 1, 1]
+k2 = 1
+
+nums3 = [1, 2, 3, 1, 2, 3]
+k3 = 2
+
+print(containsNearbyDuplicate(nums1, k1))  # True
+print(containsNearbyDuplicate(nums2, k2))  # True
+print(containsNearbyDuplicate(nums3, k3))  # False
