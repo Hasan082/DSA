8cbf026a6c554703f64c6880eae107b91b1ccc88
Md Hasanuzzaman <dr.has82@gmail.com>
2025-01-23 22:46:39 +0600

    Solution added

--- dsaPython/150Top/Palindrome_Number_9.py
+++ dsaPython/150Top/Palindrome_Number_9.py
@@ -4,3 +4,9 @@ def isPalindrome(self, x: int) -> bool:
         return True
     else:
         return False
+x1 = 121
+x2 = -121
+x3 = 10
+print(isPalindrome(x1))
+print(isPalindrome(x2))
+print(isPalindrome(x3))
