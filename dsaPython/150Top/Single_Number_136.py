28b683e1e9896d46aa9c5e70cbdae78030d09d7f
Md Hasanuzzaman <dr.has82@gmail.com>
2025-01-27 19:16:58 +0600

    solution added

--- dsaPython/150Top/Single_Number_136.py
+++ dsaPython/150Top/Single_Number_136.py
@@ -6,3 +6,8 @@ def singleNumber(nums: List[int]) -> int:
     for num in nums:
         res ^= num
     return res
+
+nums1 = [2,2,1]
+nums2 = [4,1,2,1,2]
+print(singleNumber(nums1)) # 1
+print(singleNumber(nums2)) # 4
