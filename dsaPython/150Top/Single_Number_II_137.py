e347745cd4dd0933d27eb7d628b14a1101381dab
Md Hasanuzzaman <dr.has82@gmail.com>
2025-01-27 19:32:56 +0600

    Function added

---
+++ dsaPython/150Top/Single_Number_II_137.py
@@ -0,0 +1,11 @@
+from typing import List
+from collections import Counter
+
+
+def singleNumber(nums: List[int]) -> int:
+    counter = Counter(nums)
+    res = 0
+    for key, val in counter.items():
+        if val == 1:
+            res ^= key
+    return res
