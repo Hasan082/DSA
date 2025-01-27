3cd853b05c0e3a8f73dd4fc49d5f8f74640ece47
Md Hasanuzzaman <dr.has82@gmail.com>
2025-01-27 19:35:38 +0600

    solution added

--- dsaPython/150Top/Single_Number_II_137.py
+++ dsaPython/150Top/Single_Number_II_137.py
@@ -9,3 +9,10 @@ def singleNumber(nums: List[int]) -> int:
         if val == 1:
             res ^= key
     return res
+
+
+nums1 = [2,2,3,2]
+nums2 = [0,1,0,1,0,1,99]
+
+print(singleNumber(nums1))
+print(singleNumber(nums2))
