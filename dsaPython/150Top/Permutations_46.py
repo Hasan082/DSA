1298fa70a898543d800562e09bb4a67230a8a3e8
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-02 21:45:33 +0600

    Function added

--- dsaPython/150Top/Permutations_46.py
+++ dsaPython/150Top/Permutations_46.py
@@ -4,3 +4,9 @@ import itertools
 
 def permute(nums: List[int]) -> List[List[int]]:
     return list(itertools.permutations(nums))
+
+
+nums1 = [1,2,3]
+nums2 = [0,1]
+print(permute(nums1))
+print(permute(nums2))
