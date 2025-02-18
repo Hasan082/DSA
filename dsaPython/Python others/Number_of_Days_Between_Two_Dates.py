075364964a411832d5b8a1b5ba4cd4db222495e8
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-18 22:09:00 +0600

    SOlution added

--- dsaPython/Python others/Number_of_Days_Between_Two_Dates.py
+++ dsaPython/Python others/Number_of_Days_Between_Two_Dates.py
@@ -3,5 +3,10 @@ def daysBetweenDates(date1: str, date2: str) -> int:
         y1, m1, d1 = map(int, date1.split('-'))
         y2, m2, d2 = map(int, date2.split('-'))
         d1 = date(y1, m1, d1)
-        d2 = date(y2, m2, d2)
+        d2 = date(y2, m2, d2)        
         return abs((d2 - d1).days)
+
+
+datex = "2019-06-29"
+datey = "2019-06-30"
+print(daysBetweenDates(datex, datey))
