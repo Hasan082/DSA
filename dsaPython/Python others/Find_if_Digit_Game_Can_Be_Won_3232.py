4ed4027e6869d5f592a34008a569664389d89bec
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-20 00:41:53 +0600

    Solution added

--- dsaPython/Python others/Find_if_Digit_Game_Can_Be_Won_3232.py
+++ dsaPython/Python others/Find_if_Digit_Game_Can_Be_Won_3232.py
@@ -7,3 +7,10 @@ def canAliceWin(nums: List[int]) -> bool:
         else:
             double_digit += num
     return single_digit != double_digit
+
+
+
+nums1 = [1,2,3,4,10]
+nums2 = [1,2,3,4,5,14]
+print(canAliceWin(nums1)) # False
+print(canAliceWin(nums2)) # True
