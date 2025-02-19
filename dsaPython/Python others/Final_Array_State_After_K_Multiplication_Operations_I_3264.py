1e7743ad5e4b1e141789cefeeebe6163f303fdff
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-20 00:58:45 +0600

    Solution added

--- dsaPython/Python others/Final_Array_State_After_K_Multiplication_Operations_I_3264.py
+++ dsaPython/Python others/Final_Array_State_After_K_Multiplication_Operations_I_3264.py
@@ -4,3 +4,8 @@ def getFinalState(nums: List[int], k: int, multiplier: int) -> List[int]:
         nums[nums.index(min(nums))] = min(nums) * multiplier
     return nums
 
+nums = [2, 1, 3, 5, 6]
+k = 5
+multiplier = 2
+
+print(getFinalState(nums, k, multiplier)) # [8,4,6,5,6]
