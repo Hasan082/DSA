a197e0ae3eae209b8dd920ab0d6bb8d18bbd6988
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-20 02:21:56 +0600

    Function added

---
+++ dsaPython/Python others/Minimum_Element_After_Replacement_With_Digit_Sum_3300.py
@@ -0,0 +1,10 @@
+from typing import List
+def convert_num( digit):
+    total = 0
+    while digit > 0:
+        total += digit % 10
+        digit //= 10
+    return total
+
+def minElement(nums: List[int]) -> int:
+    return min(convert_num(num) for num in nums)
