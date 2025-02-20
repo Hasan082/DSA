914e2a61dbccffb9c049abd8bbffea4e5d33cc14
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-20 14:44:09 +0600

    Solution added

--- dsaPython/Python others/Find_Unique_Binary_String_1980.py
+++ dsaPython/Python others/Find_Unique_Binary_String_1980.py
@@ -1,3 +1,9 @@
 from typing import List
 def findDifferentBinaryString(nums: List[str]) -> str:
     return "".join("1" if nums[i][i] == "0" else "0" for i in range(len(nums)))
+
+
+print(findDifferentBinaryString(["01","10"])) # 11
+print(findDifferentBinaryString(["00","01"])) # 10
+print(findDifferentBinaryString(["111","011","001"])) # 101
+print(findDifferentBinaryString(["111","011","101"])) # 010 
