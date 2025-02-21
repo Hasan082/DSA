d1fe17b5f7032140cacf3a99f420afc5a52d65fe
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-21 19:22:39 +0600

    Increasing Triplet Subsequence

--- dsaPython/main.py
+++ dsaPython/main.py
@@ -1,28 +1,29 @@
-# from pytube import YouTube
-
-#
-# video_url = 'https://www.youtube.com/watch?v=cvL1ZmUXus4'
-#
-# yt = YouTube(video_url)
-#
-# stream = yt.streams.get_highest_resolution()
-#
-# stream.download('output.mp4')
-
-# file a.py
-print("a", end='')
-    
-# file b.py
-def o(p):
-    def q():
-        return '*' * p
-    return q
+from typing import List
+def increasingTriplet(nums: List[int]) -> bool:
+    first, second = float('inf'), float('inf')
+    for num in nums:
+        if num <= first:
+            first = num
+        elif num <= second:
+            second = num
+        else:
+            return True
+    return False
 
 
-r = o(1)
-s = o(2)
-print(r() + s())
 
+nums1 = [1,2,3,4,5]
+print(increasingTriplet(nums1)) # True
+nums2 = [5,4,3,2,1]
+print(increasingTriplet(nums2)) # False
+nums3 = [2,1,5,0,4,6]
+print(increasingTriplet(nums3)) # True
+nums4 = [20,100,10,12,5,13]
+print(increasingTriplet(nums4)) # True
+nums5 = [1,1,1,1,1]
+print(increasingTriplet(nums5)) # False
+nums6 = [1,1,3,1,1,1]
+print(increasingTriplet(nums6)) # False
 
   
 
