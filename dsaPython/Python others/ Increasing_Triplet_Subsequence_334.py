017ad2ab83f68815512f1491e4af1a8330fa74e0
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-21 19:24:11 +0600

    Solution done

--- dsaPython/Python others/ Increasing_Triplet_Subsequence_334.py
+++ dsaPython/Python others/ Increasing_Triplet_Subsequence_334.py
@@ -10,3 +10,14 @@ def increasingTriplet(nums: List[int]) -> bool:
         else:
             return True
     return False
+
+nums1 = [1,2,3,4,5]
+print(increasingTriplet(nums1)) # True
+nums2 = [5,4,3,2,1]
+print(increasingTriplet(nums2)) # False
+nums3 = [2,1,5,0,4,6]
+print(increasingTriplet(nums3)) # True
+nums4 = [20,100,10,12,5,13]
+print(increasingTriplet(nums4)) # True
+nums5 = [1,1,1,1,1]
+print(increasingTriplet(nums5)) # False
