8c1c7d01dfbb5be34a55d83dbda45baf5cfa9615
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-23 23:15:21 +0600

    Solution added

--- dsaPython/Python others/ Find Numbers with Even_Number_of_Digits_1295.py
+++ dsaPython/Python others/ Find Numbers with Even_Number_of_Digits_1295.py
@@ -1,3 +1,9 @@
 from typing import List
 def findNumbers(nums: List[int]) -> int:
     return sum(1 for num in nums if len(str(num)) % 2 == 0)
+
+
+nums1 = [12,345,2,6,7896]
+print(findNumbers(nums1)) # 2
+nums2 = [555,901,482,1771]
+print(findNumbers(nums2)) # 1
