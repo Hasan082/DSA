530ff4cd7d26d1ec9b51a71fb1082865389f66ee
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-23 16:02:42 +0600

    Solution added

--- dsaPython/Python others/Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length_1456.py
+++ dsaPython/Python others/Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length_1456.py
@@ -1,4 +1,4 @@
-def maxVowels(self, s: str, k: int) -> int:
+def maxVowels(s: str, k: int) -> int:
     vowels = {'a', 'e', 'i', 'o', 'u'}
     current_sum = sum(1 for char in s[:k] if char in vowels)
     max_count = current_sum
@@ -11,3 +11,11 @@ def maxVowels(self, s: str, k: int) -> int:
         if k == current_sum:
             return k
     return max_count
+
+
+s1 = "abciiidef"
+k1 = 3
+print(maxVowels(s1, k1)) # 3
+s2 = "aeiou"
+k2 = 2
+print(maxVowels(s2, k2)) # 2
