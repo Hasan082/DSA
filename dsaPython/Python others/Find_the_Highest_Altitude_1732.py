399c1acf53d6558ee69a906e236908cf1c7d955a
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-23 16:17:43 +0600

    Function added

---
+++ dsaPython/Python others/Find_the_Highest_Altitude_1732.py
@@ -0,0 +1,8 @@
+from typing import List
+def largestAltitude(gain: List[int]) -> int:
+    max_alt = 0
+    current = 0
+    for g in gain:
+        current += g
+        max_alt = max(max_alt, current)
+    return max_alt
