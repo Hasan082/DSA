0de40a29b82de377b27de5b9a4abc34b63af293e
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-23 16:54:57 +0600

    Solution added

--- dsaPython/Python others/Find_Pivot_Index_724.py
+++ dsaPython/Python others/Find_Pivot_Index_724.py
@@ -5,3 +5,8 @@ def pivotIndex(nums: List[int]) -> int:
         if left_sum == (total_sum - left_sum - num): return i
         left_sum += num
     return -1
+
+nums1 = [1,7,3,6,5,6]
+print(pivotIndex(nums1)) # 3
+nums2 = [1,2,3]
+print(pivotIndex(nums2)) # -1
