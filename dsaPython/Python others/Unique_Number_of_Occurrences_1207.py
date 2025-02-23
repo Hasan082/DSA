c1a6c27c58b284771fefd742056abd1792a66551
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-23 22:50:47 +0600

    Solution added

--- dsaPython/Python others/Unique_Number_of_Occurrences_1207.py
+++ dsaPython/Python others/Unique_Number_of_Occurrences_1207.py
@@ -6,3 +6,10 @@ def uniqueOccurrences(arr: List[int]) -> bool:
     for val in count.values():
         s.add(val) 
     return len(s) == len(count.values())
+
+arr1 = [1,2,2,1,1,3]
+print(uniqueOccurrences(arr1)) # True
+arr2 = [1,2]
+print(uniqueOccurrences(arr2)) # False
+arr3 = [-3,0,1,-3,1,1,1,-3,10,0]
+print(uniqueOccurrences(arr3)) # True
