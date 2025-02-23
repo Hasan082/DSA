48b5c77b55226e078a342b82873edc46c4151a3a
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-23 22:33:21 +0600

    solution added

--- dsaPython/Python others/Find_the_Difference_of_Two_Arrays_2215.py
+++ dsaPython/Python others/Find_the_Difference_of_Two_Arrays_2215.py
@@ -1,3 +1,11 @@
 from typing import List
 def findDifference(nums1: List[int], nums2: List[int]) -> List[List[int]]:
     return [list(set(nums1) - set(nums2)), list(set(nums2) - set(nums1))]
+
+
+nums1 = [1,2,3]
+nums2 = [2,4,6]
+print(findDifference(nums1, nums2)) # [[5], []]
+nums3 = [1,2,3,3]
+nums4 = [1,1,2,2]
+print(findDifference(nums3, nums4)) # [[3],[]]
