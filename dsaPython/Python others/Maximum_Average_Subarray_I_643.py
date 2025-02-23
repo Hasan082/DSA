6cfa3306419ae2ab869ca7dba97d7ffb4e082e7c
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-23 08:57:59 +0600

    Solution added

--- dsaPython/Python others/Maximum_Average_Subarray_I_643.py
+++ dsaPython/Python others/Maximum_Average_Subarray_I_643.py
@@ -7,3 +7,15 @@ def findMaxAverage(nums: List[int], k: int) -> float:
         avg = max(avg, first_sum)
 
     return avg / k
+
+
+
+nums1 = [0,1,1,3,3]
+k1 = 4
+print(findMaxAverage(nums1, k1)) # 2.0
+nums2 = [1,12,-5,-6,50,3]
+k2 = 4
+print(findMaxAverage(nums2, k2)) # 12.75
+nums3 = [5]
+k3 = 1
+print(findMaxAverage(nums3, k3)) # 5.0
