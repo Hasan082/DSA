ef1d523522668461e7a6f34652ae55d065cb9e9a
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-23 16:18:55 +0600

    Solution add

--- dsaPython/Python others/Find_the_Highest_Altitude_1732.py
+++ dsaPython/Python others/Find_the_Highest_Altitude_1732.py
@@ -6,3 +6,9 @@ def largestAltitude(gain: List[int]) -> int:
         current += g
         max_alt = max(max_alt, current)
     return max_alt
+
+
+gain1 = [-5,1,5,0,-7]
+print(largestAltitude(gain1)) # 1
+gain2 = [-4,-3,-2,-1,4,3,2]
+print(largestAltitude(gain2)) # 0
