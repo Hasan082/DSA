77f794d863081b4b0a9ea984dce169612b040bfa
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-24 23:51:57 +0600

    Solution added

--- dsaPython/Python others/Minimum_Average_of_Smallest_and_Largest_Elements_3194.py
+++ dsaPython/Python others/Minimum_Average_of_Smallest_and_Largest_Elements_3194.py
@@ -5,3 +5,9 @@ def minimumAverage(nums: List[int]) -> float:
     for i in range(len(nums) // 2):
         min_avg = min(min_avg, nums[i] + nums[-i - 1])
     return min_avg / 2
+
+
+nums1 = [7,8,3,4,15,13,4,1]
+print(minimumAverage(nums1)) # 5.5
+nums2 = [1,9,8,3,10,5]
+print(minimumAverage(nums2)) # 5.5
