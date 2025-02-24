cf4676bccfc889d90f809a1406bff05f2ac1be93
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-25 00:45:02 +0600

    solution added

--- dsaPython/Python others/Maximum_Value_of_a_String_in_an_Array_2496.py
+++ dsaPython/Python others/Maximum_Value_of_a_String_in_an_Array_2496.py
@@ -1,3 +1,9 @@
 from typing import List
 def maximumValue(strs: List[str]) -> int:
     return max(int(c) if c.isdigit() else len(c) for c in strs)
+
+
+strs1 = ["alic3","bob","3","4","00000"]
+print(maximumValue(strs1)) # 5
+strs2 = ["1","01","001","0001"]
+print(maximumValue(strs2)) # 1
