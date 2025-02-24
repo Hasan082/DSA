7be98a500036742f55bfe32dcc229da88c48fb44
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-25 00:11:06 +0600

    Sol;ution added

--- dsaPython/Python others/Maximum_Sum_With_Exactly_K_Elements_2656.py
+++ dsaPython/Python others/Maximum_Sum_With_Exactly_K_Elements_2656.py
@@ -7,3 +7,9 @@ def maximizeSum(nums: List[int], k: int) -> int:
         max_num += 1
     return res
 
+nums1 = [1,2,3,4,5]
+k1 = 3
+print(maximizeSum(nums1, k1)) # 18
+nums2 = [5,5,5]
+k2 = 2
+print(maximizeSum(nums2, k2)) # 11
