f546ece22c19cdabb02afe969f721b766544748e
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-24 23:12:49 +0600

    Solution added

--- dsaPython/Python others/ Decode_XORed_Array_1720.py
+++ dsaPython/Python others/ Decode_XORed_Array_1720.py
@@ -4,3 +4,11 @@ def decode(encoded: List[int], first: int) -> List[int]:
     for num in encoded:
         arr.append(arr[-1] ^ num)
     return arr
+
+
+encoded1 = [1,2,3]
+first1 = 1
+print(decode(encoded1, first1)) # [1,0,2,3]
+encoded2 = [6,2,7,3]
+first2 = 4
+print(decode(encoded2, first2)) # [4,2,0,7,4]
