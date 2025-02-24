5aee75f8b0351f3720522e9cca0c45cd979067e7
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-25 01:45:19 +0600

    Solution added

--- dsaPython/Python others/Count_the_Number_of_Vowel_Strings_in_Range_2586.py
+++ dsaPython/Python others/Count_the_Number_of_Vowel_Strings_in_Range_2586.py
@@ -2,3 +2,13 @@ from typing import List
 def vowelStrings(words: List[str], left: int, right: int) -> int:
     vowel = set('aeiou')
     return sum({c[0],c[-1]}.issubset(vowel) for c in words[left: right + 1])
+
+
+words1 = ["are","amy","u"]
+left1 = 0
+right1 = 2
+print(vowelStrings(words1, left1, right1)) # 2
+words2 = ["hey","aeo","mu","ooo","artro"]
+left2 = 1
+right2 = 4
+print(vowelStrings(words2, left2, right2)) # 3
