3d39c09e9bfece44f8341ba1b0d0f0a4526f5294
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-27 02:54:17 +0600

    Number of Sub-arrays With Odd Sum solution addedd

--- dsaPython/Python others/Number of Sub-arrays With Odd Sum_1542.py
+++ dsaPython/Python others/Number of Sub-arrays With Odd Sum_1542.py
@@ -9,3 +9,11 @@ def numOfSubarrays(arr: List[int]) -> int:
         else:
             even += 1
     return (odd * even) % (pow(10, 9) + 7)
+
+
+arr1 = [1,3,5]
+print(numOfSubarrays(arr1)) # 4
+arr2 = [2,4,6]
+print(numOfSubarrays(arr2)) # 0
+arr3 = [1,2,3,4,5,6,7]
+print(numOfSubarrays(arr3)) # 16
