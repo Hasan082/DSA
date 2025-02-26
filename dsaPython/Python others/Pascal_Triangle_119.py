737e817e8969d9796f81febc6e33cdc07bee9077
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-26 23:41:11 +0600

    Function created

--- dsaPython/Python others/Pascal_Triangle_119.py
+++ dsaPython/Python others/Pascal_Triangle_119.py
@@ -11,12 +11,3 @@ def generate(self, numRows: int) -> List[List[int]]:
         res.append(temp)
     return res
 
-
-numRows1 = 5
-print(generate(numRows1)) # [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
-numRows2 = 1
-print(generate(numRows2)) # [[1]]
-numRows3 = 2
-print(generate(numRows3)) # [[1],[1,1]]
-numRows4 = 3
-print(generate(numRows4)) # [[1],[1,1],[1,2,1]]
