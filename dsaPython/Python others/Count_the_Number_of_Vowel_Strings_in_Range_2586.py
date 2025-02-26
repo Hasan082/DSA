15100650ccc41104e7218db187910e726c2202f3
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-26 23:05:26 +0600

    File modified

--- dsaPython/Python others/Count_the_Number_of_Vowel_Strings_in_Range_2586.py
+++ dsaPython/Python others/Count_the_Number_of_Vowel_Strings_in_Range_2586.py
@@ -4,10 +4,12 @@ def vowelStrings(words: List[str], left: int, right: int) -> int:
     return sum({c[0],c[-1]}.issubset(vowel) for c in words[left: right + 1])
 
 
+
 words1 = ["are","amy","u"]
 left1 = 0
 right1 = 2
 print(vowelStrings(words1, left1, right1)) # 2
+
 words2 = ["hey","aeo","mu","ooo","artro"]
 left2 = 1
 right2 = 4
