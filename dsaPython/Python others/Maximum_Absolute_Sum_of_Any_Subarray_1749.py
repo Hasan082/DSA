fafca934bc5c2984000f761325eada6d2a2401dd
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-27 01:37:58 +0600

    Maximum Absolute Sum of Any Subarray solution added

--- dsaPython/Python others/Maximum_Absolute_Sum_of_Any_Subarray_1749.py
+++ dsaPython/Python others/Maximum_Absolute_Sum_of_Any_Subarray_1749.py
@@ -3,3 +3,10 @@ from itertools import accumulate
 def maxAbsoluteSum(nums: List[int]) -> int:    
     l = list(accumulate(nums, initial = 0))
     return max(l) - min(l)
+
+nums1 = [1,-3,2,3,-4]
+print(maxAbsoluteSum(nums1)) # 5
+nums2 = [2,-5,1,-4,3,-2]
+print(maxAbsoluteSum(nums2)) # 8
+nums3 = [1,2,3]
+print(maxAbsoluteSum(nums3)) # 6
