e739164a4b39c10d76fe7549e8a382cd1c4de7dd
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-26 23:32:46 +0600

    Function created

---
+++ dsaPython/Python others/Pascal_Triangle_118.py
@@ -0,0 +1,12 @@
+from math import factorial
+from typing import List
+def helper(n, k):
+    return factorial(n) // (factorial(k) * factorial(n - k))
+def generate(self, numRows: int) -> List[List[int]]:
+    res = [] 
+    for n in range(numRows):
+        temp = []
+        for k in range(n + 1):
+            temp.append(self.helper(n, k))
+        res.append(temp)
+    return res
