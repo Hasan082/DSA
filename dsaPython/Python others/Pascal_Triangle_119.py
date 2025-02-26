1c88b38cf7dfd00f6c9198e8e70d062dbe75e500
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-26 23:40:55 +0600

    File created

---
+++ dsaPython/Python others/Pascal_Triangle_119.py
@@ -0,0 +1,22 @@
+from math import factorial
+from typing import List
+def helper(n, k):
+    return factorial(n) // (factorial(k) * factorial(n - k))
+def generate(self, numRows: int) -> List[List[int]]:
+    res = [] 
+    for n in range(numRows):
+        temp = []
+        for k in range(n + 1):
+            temp.append(self.helper(n, k))
+        res.append(temp)
+    return res
+
+
+numRows1 = 5
+print(generate(numRows1)) # [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+numRows2 = 1
+print(generate(numRows2)) # [[1]]
+numRows3 = 2
+print(generate(numRows3)) # [[1],[1,1]]
+numRows4 = 3
+print(generate(numRows4)) # [[1],[1,1],[1,2,1]]
