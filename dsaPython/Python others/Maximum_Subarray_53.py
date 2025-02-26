4f184766d4445e8aa857a23bf2e23c8c6634d433
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-27 01:32:54 +0600

    Maximum Subarray functioon added

---
+++ dsaPython/Python others/Maximum_Subarray_53.py
@@ -0,0 +1,16 @@
+from typing import List
+def maxSubArray(nums: List[int]) -> int:
+    max_sum = float('-inf')
+    curr_sum=0
+    for num in nums:
+        curr_sum+=num
+        if curr_sum > max_sum:
+            max_sum = curr_sum
+        if curr_sum<0:
+            curr_sum=0
+    return max_sum
+
+nums1 = [-2,1,-3,4,-1,2,1,-5,4]
+print(maxSubArray(nums1)) # 6
+nums2 = [1]
+print(maxSubArray(nums2)) # 1
