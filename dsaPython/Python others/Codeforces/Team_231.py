5f39885ce85aaee4c0b44e568b339304de5e4c44
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-27 18:21:56 +0600

    Function created

--- dsaPython/Python others/Codeforces/Team_231.py
+++ dsaPython/Python others/Codeforces/Team_231.py
@@ -1,2 +1,10 @@
 # https://codeforces.com/problemset/problem/231/A
 
+time_operation = int(input())
+count = 0
+for _ in range(time_operation):
+    a, b, c = map(int, input().split())
+    if a + b + c >= 2:
+        count += 1
+
+print(count)   
