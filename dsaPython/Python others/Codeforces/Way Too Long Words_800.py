953ae76b0f43fe639396f997f64eccdf8eb4f891
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-27 18:12:44 +0600

    Function created

--- dsaPython/Python others/Codeforces/Way Too Long Words_800.py
+++ dsaPython/Python others/Codeforces/Way Too Long Words_800.py
@@ -1,3 +1,16 @@
 # https://codeforces.com/problemset/problem/71/A
 
 
+def encode_words(words: str) -> str:
+    try:
+        if len(words) > 10:
+            return words[0] + str(len(words) - 2) + words[-1]
+        else:
+            return words
+    except (ValueError, TypeError):
+        print("Invalid Input")
+        encode_words(words)
+
+n = int(input())
+for _ in range(n):
+    print(encode_words(input()))
