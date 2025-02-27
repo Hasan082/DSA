ecb4e136c512c1f17dcdcdb664ad277a00d37978
Md Hasanuzzaman <dr.has82@gmail.com>
2025-02-27 18:53:46 +0600

    Soletion accepted

--- dsaPython/Python others/Codeforces/Next Round_158.py
+++ dsaPython/Python others/Codeforces/Next Round_158.py
@@ -1,1 +1,10 @@
 # https://codeforces.com/problemset/problem/158/A
+a, b = map(int, input().split())
+score = list(map(int, input().split()))
+target = score[b - 1]
+count = 0
+for i in score:
+    if i >=target and i > 0:
+        count += 1
+
+print(count)
