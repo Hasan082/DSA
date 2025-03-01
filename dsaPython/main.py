3c53791249358c4d09e257f76291f87ea751df57
Md Hasanuzzaman <dr.has82@gmail.com>
2025-03-01 19:54:08 +0600

    Statistick libray done

--- dsaPython/main.py
+++ dsaPython/main.py
@@ -1,37 +1,19 @@
-from typing import List
-def increasingTriplet(nums: List[int]) -> bool:
-    first, second = float('inf'), float('inf')
-    for num in nums:
-        if num <= first:
-            first = num
-        elif num <= second:
-            second = num
-        else:
-            return True
-    return False
+# Calculate mean, median, and mode
+import statistics
+
+# Data
+data = [62000, 64000, 49000, 324000, 1264000, 54330, 64000, 51000, 55000, 48000, 53000]
 
 
-
-nums1 = [1,2,3,4,5]
-print(increasingTriplet(nums1)) # True
-nums2 = [5,4,3,2,1]
-print(increasingTriplet(nums2)) # False
-nums3 = [2,1,5,0,4,6]
-print(increasingTriplet(nums3)) # True
-nums4 = [20,100,10,12,5,13]
-print(increasingTriplet(nums4)) # True
-nums5 = [1,1,1,1,1]
-print(increasingTriplet(nums5)) # False
-nums6 = [1,1,3,1,1,1]
-print(increasingTriplet(nums6)) # False
-
-  
-
-
-
-
-
-
-
-
-
+x = statistics.variance(data)
+y = statistics.stdev(data)
+z = statistics.pstdev(data)
+p = statistics.mean(data)
+q = statistics.median(data)
+r = statistics.mode(data)
+print(f"Variance {x}")
+print(f"Standard Deviation {y}")
+print(f"Population Standard Deviation {z}")
+print(f"Mean {p}")
+print(f"Median {q}")
+print(f"Mode {r}")
