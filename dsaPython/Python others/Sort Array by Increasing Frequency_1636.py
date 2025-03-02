c67ebcb6acfbe882e4adff43aa73df7504513357
Md Hasanuzzaman <dr.has82@gmail.com>
2025-03-02 23:47:07 +0600

    Solution added

--- dsaPython/Python others/Sort Array by Increasing Frequency_1636.py
+++ dsaPython/Python others/Sort Array by Increasing Frequency_1636.py
@@ -3,3 +3,8 @@ from typing import List
 def frequencySort(nums: List[int]) -> List[int]:
     count = Counter(nums)
     return sorted(nums, key=lambda x: (count[x], -x))
+
+
+
+nums = [1,1,2,2,2,3]
+print(frequencySort(nums))
