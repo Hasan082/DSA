cc955e4a2991537bda8cec97cf350a7519dba5b8
Md Hasanuzzaman <dr.has82@gmail.com>
2025-03-03 00:18:54 +0600

    SOlution added

--- dsaPython/Python others/Second Largest Digit in a String_1796.py
+++ dsaPython/Python others/Second Largest Digit in a String_1796.py
@@ -1,8 +1,12 @@
 def secondHighest(s: str) -> int:
     res = []
-    
+
     for c in s:
         if c.isdigit() and int(c) not in res:
             res.append(int(c))
     res.sort()
     return res[-2] if len(res) > 1 else -1
+
+s = "dfa12321afd"
+
+print(secondHighest(s)) # 2
