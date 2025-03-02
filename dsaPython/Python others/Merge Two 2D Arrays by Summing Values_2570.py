77fd0e2964d0e5b7a9a8bc989e9719ba8137508e
Md Hasanuzzaman <dr.has82@gmail.com>
2025-03-02 22:10:01 +0600

    Solution done

--- dsaPython/Python others/Merge Two 2D Arrays by Summing Values_2570.py
+++ dsaPython/Python others/Merge Two 2D Arrays by Summing Values_2570.py
@@ -7,5 +7,10 @@ def mergeArrays(nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int
      
     for num in arr:
         maps[num[0]] += num[1]
-        
+
     return sorted(maps.items())
+
+
+nums1 = [[1,2],[2,3],[4,5]]
+nums2 = [[1,4],[3,2],[4,1]]
+print( mergeArrays(nums1, nums2))
