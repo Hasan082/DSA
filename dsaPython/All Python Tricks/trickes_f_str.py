fcd00805a36cc62438f4251f0af7fe84c87d668e
Md Hasanuzzaman <dr.has82@gmail.com>
2025-03-04 16:53:19 +0600

    Tricks 2 done

--- dsaPython/All Python Tricks/trickes_f_str.py
+++ dsaPython/All Python Tricks/trickes_f_str.py
@@ -1,5 +1,19 @@
+# TRICKS 1
 n: int = 1_000_000_000
 print(n)
 n : int = 1000000000
 print(f"{n:_}") # separated by underscore
 print(f"{n:,}") # separted by coma(,)
+
+
+
+# TRICKS 2
+
+var: str = 'abc'
+
+print(f"{var:>20}:") # make space before 
+print(f"{var:<20}:") # make space after 
+print(f"{var:^20}:") # make both side and text is middle
+
+
+# TRICKS 3
