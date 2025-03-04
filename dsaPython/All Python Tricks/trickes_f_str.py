2d8f19232a67e174d4f4f9b90e8e852df15fb400
Md Hasanuzzaman <dr.has82@gmail.com>
2025-03-04 19:17:23 +0600

    Tricks 3 done

--- dsaPython/All Python Tricks/trickes_f_str.py
+++ dsaPython/All Python Tricks/trickes_f_str.py
@@ -16,4 +16,14 @@ print(f"{var:<20}:") # make space after
 print(f"{var:^20}:") # make both side and text is middle
 
 
-# TRICKS 3
+# TRICKS 3 DATETIME
+from datetime import datetime
+
+now: datetime = datetime.now()
+
+print(f"{now:%d.%m.%y}") # 'y' Year is short form like 04.03.25
+print(f"{now:%d.%m.%Y}") # by 'Y' Year is Full form form like 04.03.2025
+print(f"{now:%d.%m.%Y (%H.%M.%S)}") # by '(%H.%M.%S)' Hour minute sec
+print(f"{now:%c}") # Full Specification about date
+print(f"{now:%I%p}") # Date and TIME AM/PM formate
+
