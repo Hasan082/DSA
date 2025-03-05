28f3f3ec940bef20acc28d4c36cc55515cffd53b
Md Hasanuzzaman <dr.has82@gmail.com>
2025-03-05 23:05:36 +0600

    set one operation done

--- dsaPython/All Python Tricks/python_5_tricks.py
+++ dsaPython/All Python Tricks/python_5_tricks.py
@@ -14,3 +14,12 @@ print(text[f_five])
 print(numbers[::-1])
 print(text[::-1])
 
+
+# SET TRICKS
+
+set_a: set[int] = {1, 2, 3, 4, 5}
+set_b: set[int] = {4, 5, 6, 7, 8}
+
+print(set_a | set_b)
+set_a.update(set_b)
+print(set_a)
