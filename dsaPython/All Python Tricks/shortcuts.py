66f21a6fbea641fb81a2ad7e59b9deb92cbdb93e
Md Hasanuzzaman <dr.has82@gmail.com>
2025-03-09 23:16:21 +0600

    Unpacking done

--- dsaPython/All Python Tricks/shortcuts.py
+++ dsaPython/All Python Tricks/shortcuts.py
@@ -6,3 +6,27 @@ z = f'Hello {name} {[1, 2, 3]}'
 x = F'Hello {name} {[1, 2, 3]}'
 print(z)
 print(x)
+
+
+# Unpacking
+
+tup = (1, 2, 3, 4, 5)
+lst = [1, 2, 3, 4, 5]
+string = "hello"
+dic = {'a': 1, "b": 2}
+coords = [4, 5]
+
+a, b, c, d, e = tup
+print(a, b, c, d, e)
+f, g, h, i, j = lst
+print(f, g, h, i, j)
+k, l, m, n, o = string
+print(k, l, m, n, o)
+p, q = dic.values()
+print(p, q)
+r, s = dic.keys()
+print(r, s)
+t, u = dic.items()
+print(t, u)
+x, y = coords
+print(x, y)
