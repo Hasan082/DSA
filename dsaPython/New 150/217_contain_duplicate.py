21d61b0b7ad6db8075e94933a3b16a312eb9fd3f
Md Hasanuzzaman <dr.has82@gmail.com>
2025-03-23 00:33:02 +0600

    Contain duplicate 2 done

---
+++ dsaPython/New 150/217_contain_duplicate.py
@@ -0,0 +1,10 @@
+def containDuplicates_2(nums: list, k: int) -> bool:
+    seen = {}
+    for i, num in enumerate(nums):
+        if num in seen and abs(seen[num]-i) <= k:
+            return True
+        seen[num] = i
+    return False
+
+
+print(containDuplicates_2([1, 2, 3, 4, 2], 3))
