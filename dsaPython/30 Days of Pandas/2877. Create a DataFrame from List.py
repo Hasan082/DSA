4817bbbc424caa3aac036de1bd09f40161bda848
Md Hasanuzzaman <dr.has82@gmail.com>
2025-03-23 02:14:53 +0600

    Study plan added

--- dsaPython/30 Days of Pandas/2877. Create a DataFrame from List.py
+++ dsaPython/30 Days of Pandas/2877. Create a DataFrame from List.py
@@ -25,3 +25,7 @@ import pandas as pd
 
 def createDataframe(student_data: list[list[int]]) -> pd.DataFrame:
     return pd.DataFrame(student_data, columns=["student_id", "age"])
+
+# Todo
+# https://leetcode.com/studyplan/introduction-to-pandas/
+# https://leetcode.com/studyplan/30-days-of-pandas/
