6545a71019e286aba6415392e1add74665f48695
Md Hasanuzzaman <dr.has82@gmail.com>
2025-03-24 00:50:17 +0600

    88. Merge Sorted Array

---
+++ dsaPython/New 150/88. Merge Sorted Array.py
@@ -0,0 +1,14 @@
+def merge(nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+    """
+    Do not return anything, modify nums1 in-place instead.
+    """
+    for i in range(m, len(nums1)):
+        nums1[i] = nums2[i - m]
+    nums1.sort()
+
+
+nums1 = [1, 2, 3, 0, 0, 0]
+m = 3
+nums2 = [2, 5, 6]
+n = 3
+print(merge(nums1, m, nums2, n))
