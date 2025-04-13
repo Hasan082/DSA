4e62ed4f2263b6b077a056cde07a685714f7a22d
Md Hasanuzzaman <dr.has82@gmail.com>
2025-04-13 18:29:40 +0600

    Even Numbers

---
+++ codeforces/Even_Numbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Even_Numbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        if (t == 1) System.out.println(-1);
+        for (int i = 1; i <= t; i ++) {
+            if (i % 2 == 0){
+                System.out.println(i);
+            };
+        }
+        
+        scanner.close();
+    }
+}
