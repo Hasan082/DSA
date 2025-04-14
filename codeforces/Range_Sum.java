c019fccdd459871f3afdd84abea5004ed7860a55
Md Hasanuzzaman <dr.has82@gmail.com>
2025-04-15 04:25:45 +0600

    Range sum

---
+++ codeforces/Range_Sum.java
@@ -0,0 +1,27 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Range_Sum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        while (t-- > 0) {
+            BigInteger a = scanner.nextBigInteger();
+            BigInteger b = scanner.nextBigInteger();
+
+            if (a.compareTo(b) > 0) {
+                BigInteger temp = a;
+                a = b;
+                b = temp;
+            }
+
+            BigInteger sumB = b.multiply(b.add(BigInteger.ONE)).divide(BigInteger.TWO);
+            BigInteger sumA = a.multiply(a.add(BigInteger.ONE)).divide(BigInteger.TWO);
+            BigInteger result = sumB.subtract(sumA);
+
+            System.out.println(result.add(a));
+        }
+        scanner.close();
+    }
+}
+
