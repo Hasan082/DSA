be9f452904e0a53fd5b8b2fc0117ba3c7fdf7bd3
Md Hasanuzzaman <dr.has82@gmail.com>
2025-05-06 22:30:25 +0600

    Leat code 33 done and optimized

--- dsaPython/main.py
+++ dsaPython/main.py
@@ -1,19 +1,8 @@
-# Calculate mean, median, and mode
-import statistics
-
-# Data
-data = [62000, 64000, 49000, 324000, 1264000, 54330, 64000, 51000, 55000, 48000, 53000]
+from typing import List
 
 
-x = statistics.variance(data)
-y = statistics.stdev(data)
-z = statistics.pstdev(data)
-p = statistics.mean(data)
-q = statistics.median(data)
-r = statistics.mode(data)
-print(f"Variance {x}")
-print(f"Standard Deviation {y}")
-print(f"Population Standard Deviation {z}")
-print(f"Mean {p}")
-print(f"Median {q}")
-print(f"Mode {r}")
+def search(nums: List[int], target: int) -> int:
+        if target in nums:
+            return nums.index(target)
+        else:
+              return -1
