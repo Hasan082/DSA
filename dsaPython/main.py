e76fcb05a65f57b17bb5799324f0a798c8fbfb23
Md Hasanuzzaman <dr.has82@gmail.com>
2025-05-25 00:17:43 +0600

    pull done

--- dsaPython/main.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-
-
-def search(nums: List[int], target: int) -> int:
-        if target in nums:
-            return nums.index(target)
-        else:
-              return -1
